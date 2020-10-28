5ac14fe7bdf6df11397b3fbfd515ef4b301ec34a
Silvana <silvanadinha@yahoo.com>
2020-10-28 17:06:16 +0100

    kommentar

--- src/components/seminarier.js
+++ src/components/seminarier.js
@@ -9,22 +9,18 @@ function Seminars() {
         <li>
           <Link to="/sem1">Crystal Reports</Link>
         </li>
-        <p>
-          - här kan du läsa om Nusmir tryck på Presentationsteknik för [mer
-          info]
-        </p>
+        <p>- Här kan du läsa mer om Crystal Reports.</p>
         <li>
           <Link to="/sem2">KA-Dagen</Link>
         </li>
         <p>
-          - här kan läsa omfggdsfgdafbgdfbfdb tgqertg gra ggar rg rgergr <br />
-          gdrdrhrgherhg h herhrehre htrhthr thr
+          - Här kan du läsa mer om hur KA-Dagen går till.
           <br />
         </p>
         <li>
           <Link to="/sem3">Våga Tala</Link>
         </li>
-        <p>- här kan du läsa om fgkdsf fdgjfd gfd gdfdfm</p>
+        <p>- Här kan du läsa mer om....</p>
         <li>
           <Link to="/" style={{ fontSize: "25px" }}>
             HOME
